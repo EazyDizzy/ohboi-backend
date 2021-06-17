db985f62dcd8f64a418df59b523d9a2cf213b7e0
e33 <vitalii.svinchyak@gmail.com>
2021-06-17 17:04:46 +0300

    Http validation (#9)

--- src/http/product.rs
+++ src/http/product.rs
@@ -1,6 +1,7 @@
 use actix_web::HttpResponse;
-use actix_web_validator::{Json, Validate};
+use actix_web_validator::Json;
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 use crate::http::db::product::repository::get_all_products_of_category;
 
@@ -11,10 +12,10 @@ pub async fn get_products(filters: Json<ProductFilters>) -> HttpResponse {
 
 #[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct ProductFilters {
-    #[validate(length(min = 1, max = 1000))]
+    #[validate(length(min = 1, max = 1000, message = "should have length from 1 to 1000"))]
     pub title: Option<String>,
-    #[validate(length(min = 1, max = 1000))]
+    #[validate(length(min = 1, max = 1000, message = "should be an array of 1-1000 elements"))]
     pub category: Option<Vec<i32>>,
-    #[validate(range(min = 0, message = "Page should be bigger than zero"), range(max = 4294967295, message = "Page should be less than 4294967295"))]
+    #[validate(range(min = 0, message = "should be bigger than or equal to zero"), range(max = 4294967295, message = "should be less than 4294967295"))]
     pub page: u32,
 }
