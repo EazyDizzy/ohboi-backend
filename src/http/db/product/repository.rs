7d5f8da363469d5b4d8beb703f745e9f55d88013
e33 <vitaliy.svinchyak@gmail.com>
2021-06-24 10:02:32 +0300

    filter by source id

--- src/http/db/product/repository.rs
+++ src/http/db/product/repository.rs
@@ -1,20 +1,36 @@
 use diesel::{QueryDsl, RunQueryDsl};
 
-use crate::diesel::prelude::*;
 use crate::common::db;
+use crate::diesel::prelude::*;
 use crate::http::db::lower;
 use crate::http::db::product::entity::Product;
 use crate::http::product::ProductFilters;
 use crate::schema::product;
 use crate::schema::product::dsl::{category, enabled, id, title};
+use crate::schema::source_product;
 
-pub fn get_all_products_of_category(filters: &ProductFilters) -> Vec<Product> {
+pub fn get_filtered_products(filters: &ProductFilters) -> Vec<Product> {
     let connection = &db::establish_connection();
 
-    let mut query = product::table
+    let mut query = product::table.left_join(source_product::table)
+        .select((
+            product::id,
+            product::title,
+            product::description,
+            product::lowest_price,
+            product::highest_price,
+            product::images,
+            product::category,
+            product::enabled,
+            product::created_at,
+            product::updated_at,
+        ))
         .into_boxed();
 
-    query = query.filter(enabled.eq(true));
+    query = query.filter(enabled.eq(true))
+        .limit(20)
+        .offset((filters.page * 20).into())
+        .order(id.asc());
 
     if filters.title.is_some() {
         let requested_title = filters.title.clone().unwrap().to_lowercase();
@@ -31,10 +47,12 @@ pub fn get_all_products_of_category(filters: &ProductFilters) -> Vec<Product> {
         );
     }
 
+    if filters.source.is_some() {
+        query = query
+            .filter(source_product::source_id.eq_any(filters.source.clone().unwrap()));
+    }
+
     query
-        .limit(20)
-        .offset((filters.page * 20).into())
-        .order(id.asc())
         .load::<Product>(connection)
         .expect("Error loading products")
 }
